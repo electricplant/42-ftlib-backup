c2d884f925dc61e68370ecd76bfbb0f87a5b9195
Dean Gerhard <dgerhard@bess-f4r1s2.clusters.42paris.fr>
2023-11-09 16:07:51 +0100

    hi 2

---
+++ functions for lib/ft_strrchr.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strrchr.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dgerhard <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/09 16:01:48 by dgerhard          #+#    #+#             */
+/*   Updated: 2023/11/09 16:01:53 by dgerhard         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strrchr(const char *s, int c)
+{
+	ssize_t	len;
+
+	len = (ssize_t)ft_strlen((char *)s);
+	while (len >= 0)
+	{
+		if (s[len] == (char)c)
+			return ((char *)&s[len]);
+		len--;
+	}
+	return (NULL);
+}
